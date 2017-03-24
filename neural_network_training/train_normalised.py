fd1e570f1179019a9b499d9dd1e87741fc0655f2
LetiP <leti_parcalabescu@yahoo.com>
2017-03-24 01:02:29 +0100

    Add license

--- neural_network_training/train_normalised.py
+++ neural_network_training/train_normalised.py
@@ -1,9 +1,22 @@
-# coding: utf-8
+"""
+Training a neural network on normalised images of CelebA dataset.
+http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
+Copyright (C) 2017 Letitia Parcalabescu
 
-# import theano
-# theano.config.device = 'gpu'
-# theano.config.floatX = 'float32'
-# import tensorflow as tf
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 from util.progressbar import ProgressBar
 np.random.seed(1337)  # for reproducibility
